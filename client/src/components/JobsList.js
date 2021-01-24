7e7a299abf7572fa23b5f112afe9802c1fd125f0
Araceli <begonaguereca@gmail.com>
2021-01-23 20:04:46 -0800

    update and delete

--- client/src/components/JobsList.js
+++ client/src/components/JobsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import NewJobForm from "./NewJobForm";
 import Job from "./Job";
+import EditJobForm from "./EditJobForm";
 
 const JobsList = (props) => {
   useEffect(() => {
@@ -10,13 +11,15 @@ const JobsList = (props) => {
     });
   }, []);
 
-  const [jobs, setJobs] = useState([]);
-
   const initialFormState = {
     company: "",
     position: "",
     description: ""
   };
+
+  const [jobs, setJobs] = useState([]);
+  const [editing, setEditing] = useState(false);
+  const [currentJob, setCurrentJob] = useState(initialFormState);
 
   const addJob = (job) => {
     const qs = require("qs");
@@ -37,19 +40,69 @@ const JobsList = (props) => {
     setJobs([...jobs, job]);
   };
 
+  const removeJob = (id) => {
+    axios
+      .delete("/api/v1/jobs/" + id)
+      .then((response) => {
+        setJobs(jobs.filter((job) => job.id !== id));
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const editJob = (job) => {
+    setEditing(true);
+    setCurrentJob({
+      id: job.id,
+      company: job.company,
+      position: job.position,
+      description: job.description
+    });
+  };
+
+  const updateJob = (updatedJob) => {
+    setEditing(false);
+
+    const qs = require("qs");
+    axios
+      .patch(
+        "/api/v1/jobs/" + updatedJob.id,
+        qs.stringify({
+          job: {
+            company: updatedJob.company,
+            position: updatedJob.position,
+            description: updatedJob.description
+          }
+        })
+      )
+      .then((res) => console.log(res.data));
+
+    setJobs(jobs.map((job) => (job.id === updatedJob.id ? updatedJob : job)));
+  };
+
   return (
     <div>
       <div className="jobs-list">
         <div>
-          <NewJobForm addJob={addJob} initialFormState={initialFormState} />
+          {editing ? (
+            <EditJobForm
+              setEditing={setEditing}
+              currentJob={currentJob}
+              updateJob={updateJob}
+            />
+          ) : (
+            <NewJobForm addJob={addJob} initialFormState={initialFormState} />
+          )}
         </div>
         <br />
         <hr />
-        <div>
-          {jobs.map((job, i) => (
-            <Job job={job} key={i} />
-          ))}
-        </div>
+        {jobs.map((job, _) => (
+          <Job
+            job={job}
+            removeJob={removeJob}
+            editJob={editJob}
+            editing={editing}
+          />
+        ))}
       </div>
     </div>
   );
